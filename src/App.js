f43b8a1001fcfa98980850957bae87d70165ae6f
Guilherme Matheus Corrêa <guilherme.matheus.correa@gmail.com>
2020-03-03 14:05:29 -0300

    feat: useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [techs, setTechs] = useState(['Node.js', 'React']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAddTech() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li>{tech}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAddTech}>
+        Adicionar
+      </button>
+    </>
   );
 }
 
